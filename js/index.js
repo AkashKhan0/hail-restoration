e29a946732a88649986998ef459a5e76d8a1f390
Akash khan <75297677+AkashKhan0@users.noreply.github.com>
2023-02-14 01:57:44 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function showHide() {
+    var x = document.getElementById("mobile_view");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
